fcb20b700f21e9a00d5104f2c5446bb696bb47dd
Johan Ramirez <johan911019@gmail.com>
2022-01-29 11:57:20 -0500

    Style: Se agrega plantilla de color

--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,32 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
 import App from './App'
+import { ThemeProvider, createTheme } from '@mui/material/styles'
+
+const plantilla = createTheme({
+  palette: {
+    primary: {
+      main: '#140d4fff'
+    },
+    secondary: {
+      main: '#53acf1ff'
+    },
+    info: {
+      main: '#f0f2f1ff'
+    },
+    success: {
+      main: '#f58442ff'
+    },
+    error: {
+      main: '#f7cc24ff'
+    }
+  }
+})
+
 
 ReactDOM.render(
-  <React.StrictMode>
+  <ThemeProvider theme={plantilla}>
     <App />
-  </React.StrictMode>,
+  </ThemeProvider>,
   document.getElementById('root')
 )
