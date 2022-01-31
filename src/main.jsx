f0317d56f87b07ca77f93fb89c57b5350741f866
Johan Ramirez <johan911019@gmail.com>
2022-01-31 12:49:53 -0500

    perf: Se añaden librerias y store provider

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom'
 import './index.css'
 import App from './App'
 import { ThemeProvider, createTheme } from '@mui/material/styles'
+import { Provider } from 'react-redux'
+import configureStore from './redux/app/store'
 
 const plantilla = createTheme({
   palette: {
@@ -26,14 +28,18 @@ const plantilla = createTheme({
   typography: {
     fontFamily: [
       'Roboto',
-      'open-sans',
+      'open-sans'
     ].join(',')
   }
 })
 
+const store = configureStore()
+
 ReactDOM.render(
-  <ThemeProvider theme={plantilla}>
-    <App />
-  </ThemeProvider>,
+  <Provider store={store}>
+    <ThemeProvider theme={plantilla}>
+      <App />
+    </ThemeProvider>
+  </Provider>,
   document.getElementById('root')
 )
