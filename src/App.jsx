c76b9b039a5eeca2e54b4cb8a26e295448a49ef7
davidangarita1 <davidangarita.sistemas@gmail.com>
2022-01-30 20:47:52 -0500

    Se sube test entorno

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
 
 function App () {
+  const test = import.meta.env.VITE_TEST || 123
   return (
-    <h1 className='text-9xl'>test</h1>
+    <>
+      <h1 className='text-9xl'>test</h1>
+      <h2>{test}</h2>
+    </>
+
   )
 }
 
