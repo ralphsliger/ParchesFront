9ceaefc6342a2d5615ff051587d0c99d569e1914
sebas <sebaslpez0519@gmail.com>
2022-02-01 17:32:50 -0500

    HU006: Home Page sin redireccionamiento

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,8 @@ import TestPagePrivate from './pages/private/TestPagePrivate'
 import TestPublicPage from './pages/public/TestPublicPage'
 import PublicLayout from './layouts/PublicLayout'
 import PrivateLayout from './layouts/PrivateLayout'
-import HomePage from './pages/HomePage'
+import HomePagePublic from './pages/public/HomePagePublic'
+import HomePagePrivate from './pages/private/HomePagePrivate'
 import MisParchesPage from './pages/private/MisParchesPage'
 import UnParchePagePrivate from './pages/private/UnParchePagePrivate'
 
@@ -14,9 +15,10 @@ function App () {
         <Routes>
           <Route path='/public' element={<PublicLayout />}>
             <Route path='' element={<TestPublicPage />} />
-            <Route path='home' element={<HomePage />} />
+            <Route path='home' element={<HomePagePublic />} />
           </Route>
           <Route path='/private' element={<PrivateLayout />}>
+            <Route path='home' element={<HomePagePrivate />} />
             <Route path='mis-parches' element={<MisParchesPage />} />
             <Route path='' element={<TestPagePrivate />} />
             <Route path='parche/:id' element={<UnParchePagePrivate />} />
