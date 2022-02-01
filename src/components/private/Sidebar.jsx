1b5390a0bf3900fc340daae504b2d4fbb7fcff0d
iSebastianCabrera <jscrojas25@yahoo.es>
2022-02-01 15:40:32 -0500

    public layaout ok

---
+++ src/components/private/Sidebar.jsx
@@ -0,0 +1,90 @@
+import React, { useState } from 'react'
+import { NavLink } from 'react-router-dom'
+import LogoutRoundedIcon from '@mui/icons-material/LogoutRounded'
+
+
+const Logo = () => {
+  return (
+    <div className='w-full flex flex-col items-center justify-center mt-10 mb-16'>
+      <img className=' w-28 h-28' src='https://firebasestorage.googleapis.com/v0/b/parches-62cbd.appspot.com/o/IconoParches.png?alt=media&token=076fdafd-4be9-418d-8aa3-ffe44b0c9044' />
+    </div>
+  )
+}
+
+const Sidebar = () => {
+  const [open, setOpen] = useState(true)
+  return (
+    <div className='flex flex-col md:flex-row flex-no-wrap md:h-full'>
+
+      <div className='sidebar hidden md:flex w-56 ml-6 my-3 '>
+
+        <div className='w-full h-full flex flex-col'>
+          <Logo />
+          <ul className='h-full flex flex-col '>
+            <SidebarRoute to='page/estudiantes/proyectos' title='Mi perfil' icon='fas fa-user-cog fa-lg' />
+            <SidebarRoute to='page/lider/proyectos' title='Mis parches' icon='fas fa-people-arrows fa-lg' />
+            <SidebarRoute to='page/lider/estudiantes' title='Parches' icon='fas fa-users fa-lg' />
+
+            <div className=' flex flex-col text-white h-full justify-end mb-12 mx-4 '>
+              <li className=' sidebar-route-disable sidebar-route' to='/'>
+                <NavLink
+                  to='/' onClick={() => {
+                  }}
+                >
+                  <div className='flex'>
+                    <LogoutRoundedIcon className='text-white ' />
+                    <span className='text-sm self-center ml-2 font-semibold'>Cerrar Sesión</span>
+                  </div>
+                </NavLink>
+              </li>
+
+            </div>
+          </ul>
+        </div>
+      </div>
+      <div className='flex md:hidden w-2 justify-between bg-gray-800 p-2 text-white'>
+        <i className={`fas fa-${open ? 'times' : 'bars'}`} onClick={() => setOpen(!open)} />
+        <i className='fas fa-home' />
+      </div>
+      {open && <ResponsiveSidebar />}
+
+    </div>
+  )
+}
+
+const ResponsiveSidebar = () => {
+  return (
+    <div>
+      <div
+        className='sidebar h-full z-40 absolute md:h-full sm:hidden transition duration-150 ease-in-out'
+        id='mobile-nav'
+      >
+        <div className='px-8'>
+          <Logo />
+        </div>
+      </div>
+    </div>
+  )
+}
+
+const SidebarRoute = ({ to, title, icon }) => {
+  return (
+    <li className=' mx-4 mb-3 text-xs'>
+      <NavLink
+        to={to}
+        className={({ isActive }) =>
+          isActive
+            ? 'sidebar-route sidebar-route-active text-white '
+            : 'sidebar-route sidebar-route-disable  text-white hover:text-white '}
+      >
+        <div className='flex items-center'>
+          <i className={icon} />
+          <span className='text-sm ml-2 font-semibold'>{title}</span>
+
+        </div>
+      </NavLink>
+    </li>
+  )
+}
+
+export default Sidebar
--- src/layouts/PrivateLayout.jsx
+++ src/layouts/PrivateLayout.jsx
@@ -1,15 +1,22 @@
 
 import { Outlet } from 'react-router-dom'
+import Sidebar from '../components/private/Sidebar'
+import Footer from '../components/public/Footer'
 
 const PrivateLayout = () => {
   return (
-    <>
-
-      <Outlet />
-
-    </>
+    <div className='flex flex-col md:flex-row flex-no-wrap h-screen'>
+      <Sidebar />
+      <div className='flex w-full h-full'>
+        <div className='w-full h-full overflow-y-scroll '>
+          <div className='flex flex-col p-10'>
+            <Outlet />
+            <Footer />
+          </div>
+        </div>
+      </div>
+    </div>
 
   )
 }
 export default PrivateLayout
-
