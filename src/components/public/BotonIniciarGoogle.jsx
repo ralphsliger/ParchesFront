5bca59ee134b55ac8c9768891195b8afda535f87
iSebastianCabrera <jscrojas25@yahoo.es>
2022-02-02 12:53:59 -0500

    reparando login  + integracion con ralph y johan

--- src/components/public/BotonIniciarGoogle.jsx
+++ src/components/public/BotonIniciarGoogle.jsx
@@ -1,5 +1,5 @@
 import { Button } from '@mui/material'
-import React, { useEffect } from 'react'
+import { useEffect } from 'react'
 import GoogleIcon from '@mui/icons-material/Google'
 import { useDispatch } from 'react-redux'
 import { google, app } from '../../services/firebase'
@@ -8,25 +8,21 @@ import { obtenerUsuario } from '../../redux/middlewares/obtenerUsuario'
 import useStyles from '../../utils/materialStyles'
 import { useNavigate } from 'react-router-dom'
 
-export default function BotonInicioGoogle() {
+export default function BotonInicioGoogle () {
   const auth = app.auth()
   const dispatch = useDispatch()
   const navigate = useNavigate()
   const classes = useStyles()
 
-  function IniciarSesion() {
+  function IniciarSesion () {
     auth.signInWithPopup(google)
+      .then(user => {
+        dispatch(obtenerUsuario(user.user.multiFactor.user.uid))
+        navigate('/private/inicio')
+      }).catch(error => {
+        console.log(error)
+      })
   }
-
-  useEffect(() => {
-    app.auth().onAuthStateChanged((usuario) => {
-      if (usuario) {
-        const uid = usuario.multiFactor.user.uid
-        dispatch(obtenerUsuario(uid))
-        navigate('/private')
-      }
-    })
-  }, [])
 
   return (
     <Button
--- src/components/public/InicioSesion.jsx
+++ src/components/public/InicioSesion.jsx
@@ -1,45 +1,42 @@
-import React, { useState } from "react";
-import { app } from "../../services/firebase";
-import "firebase/firestore";
-import "firebase/auth";
-import axios from "axios";
-import { inicioSesion } from "../../redux/actions/registro/registroActions";
-import { useDispatch } from "react-redux";
-import { styles } from "../../utils/registro/styles";
-import { useNavigate } from "react-router-dom";
-import BotonInicioGoogle from "./BotonRegistroGoogle";
-import { Button } from "@mui/material";
-import { Typography } from "@mui/material";
-import { TextField } from "@mui/material";
-import Container from "@mui/material/Container";
-import { Avatar } from "@mui/material";
-import { CssBaseline } from "@mui/material";
-import { LockOutlined } from "@mui/icons-material";
-import { Box } from "@mui/system";
-import { Link } from "@mui/material";
+import React, { useState } from 'react'
+import { app } from '../../services/firebase'
+import 'firebase/firestore'
+import 'firebase/auth'
+import axios from 'axios'
+import { inicioSesion } from '../../redux/actions/registro/registroActions'
+import { useDispatch } from 'react-redux'
+import { styles } from '../../utils/registro/styles'
+import { useNavigate } from 'react-router-dom'
+import BotonInicioGoogle from './BotonIniciarGoogle'
+import { Button, Typography, TextField, Avatar, CssBaseline, Link } from '@mui/material'
 
-const URL_API = "http://localhost:8080"; //Cambiar por la del back
+import Container from '@mui/material/Container'
+
+import { LockOutlined } from '@mui/icons-material'
+import { Box } from '@mui/system'
+
+const URL_API = 'http://localhost:8080' // Cambiar por la del back
 
 const InicioSesion = () => {
   const [password, setPassword] = useState('')
   const [email, setEmail] = useState('')
 
-  const dispatch = useDispatch();
-  const navigate = useNavigate();
+  const dispatch = useDispatch()
+  const navigate = useNavigate()
 
   const handleIngreso = async (event) => {
-    event.preventDefault();
-    var respuesta;
-    //TODO: Redireccionar a la página nueva
-    const auth = app.auth();
+    event.preventDefault()
+    let respuesta
+    // TODO: Redireccionar a la página nueva
+    const auth = app.auth()
     const user = await auth
       .signInWithEmailAndPassword(email, password)
-      .then((userResponse) => userResponse.user);
+      .then((userResponse) => userResponse.user)
 
     try {
       respuesta = await axios
         .get(`${URL_API}/inicioSesion/${user.uid}`)
-        .then((data) => data.data);
+        .then((data) => data.data)
       dispatch(
         inicioSesion(
           respuesta.uid,
@@ -47,74 +44,74 @@ const InicioSesion = () => {
           respuesta.nombres,
           respuesta.imagenUrl
         )
-      );
-      navigate("/private");
+      )
+      navigate('/private')
     } catch (e) {
-      respuesta = "error";
+      respuesta = 'error'
     }
-    return respuesta;
-  };
+    return respuesta
+  }
 
   return (
-    <Container component="main" maxWidth="xs">
+    <Container component='main' maxWidth='xs'>
       <CssBaseline />
       <Box
         sx={{
           marginTop: 5,
           marginBottom: 10,
-          display: "flex",
-          flexDirection: "column",
-          alignItems: "center",
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center'
         }}
       >
-        <Avatar sx={{ m: 1, bgcolor: "primary" }}>
+        <Avatar sx={{ m: 1, bgcolor: 'primary' }}>
           <LockOutlined />
         </Avatar>
-        <Typography component="h2" variant="h6">
+        <Typography component='h2' variant='h6'>
           Inicio de Sesión
         </Typography>
-        <Box component="form" sx={{ mt: 1 }}>
+        <Box component='form' sx={{ mt: 1 }}>
           <TextField
             required
             fullWidth
-            type="text"
-            id="emailIngreso"
-            label="Email"
-            autoComplete="email"
+            type='text'
+            id='emailIngreso'
+            label='Email'
+            autoComplete='email'
             sx={{ mt: 1, mb: 2 }}
             onChange={(event) => {
-              setEmail(event.target.value);
+              setEmail(event.target.value)
             }}
           />
           <TextField
             required
             fullWidth
-            type="password"
-            id="claveIngreso"
-            label="Contraseña"
+            type='password'
+            id='claveIngreso'
+            label='Contraseña'
             onChange={(event) => {
-              setPassword(event.target.value);
+              setPassword(event.target.value)
             }}
           />
           <Button
-            color="primary"
+            color='primary'
             fullWidth
-            variant="contained"
+            variant='contained'
             onClick={handleIngreso}
-            sx={{ mt: 3, mb: 1, "&:hover": { backgroundColor: "#f58442ff" } }}
+            sx={{ mt: 3, mb: 1, '&:hover': { backgroundColor: '#f58442ff' } }}
           >
             Ingresar
           </Button>
           <BotonInicioGoogle />
           <Box sx={{ mt: 2 }}>
-            <Link href="crear-cuenta" variant="body2">
-              {"¿No tienes cuenta? crea una..."}
+            <Link href='crear-cuenta' variant='body2'>
+              ¿No tienes cuenta? crea una...
             </Link>
           </Box>
         </Box>
       </Box>
     </Container>
-  );
-};
+  )
+}
 
 export default InicioSesion
