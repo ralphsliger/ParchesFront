21b4cfe0cf3e2ec3436f617955ef8e23ac6a4d9f
sebas <sebaslpez0519@gmail.com>
2022-02-02 12:41:15 -0500

    Pagina de perfil y modales

--- src/pages/private/VerPerfil.jsx
+++ src/pages/private/VerPerfil.jsx
@@ -1,5 +1,9 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
+import Typography from "@mui/material/Typography";
+import ManoIzquierda from '../../Assents/Mano1.png'
+import ModeEditIcon from '@mui/icons-material/ModeEdit';
+import IconButton from '@mui/material/IconButton'
 
 const VerPerfil = () => {
 
@@ -8,7 +12,20 @@ const VerPerfil = () => {
     console.log(auth);
 
 
-  return <div>{auth.email && <h1>{auth.email}</h1>}</div>;
+  return (  <div>
+    <div><Typography variant='h3' color='primary'>Perfíl</Typography></div>
+    <img style={{width:"80px",margin:"20px"}} src={ManoIzquierda}></img>
+    <div style={{margin:"10px"}}>
+        <Typography variant='h5' color='primary' >Correo</Typography>
+        {auth.email && <Typography variant='h7' >{auth.email}</Typography>}
+    </div>
+    <div style={{margin:"10px"}}>
+        <Typography variant='h5' color='primary'>Nombre</Typography>
+        {auth.nombres && <Typography variant='h7'>{auth.nombres}</Typography>}
+        <IconButton color='primary'><ModeEditIcon/></IconButton> 
+        </div>
+    </div>
+    )
 };
 
 export default VerPerfil;
