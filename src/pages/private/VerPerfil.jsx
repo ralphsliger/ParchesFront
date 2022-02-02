5b769da0c52678063cc416bfac2fcb61cbc20fbb
iSebastianCabrera <jscrojas25@yahoo.es>
2022-02-02 14:31:48 -0500

    quedo melo

--- src/pages/private/VerPerfil.jsx
+++ src/pages/private/VerPerfil.jsx
@@ -1,31 +1,30 @@
-import React from 'react';
-import { useSelector } from 'react-redux';
-import Typography from "@mui/material/Typography";
+import React from 'react'
+import { useSelector } from 'react-redux'
+import Typography from '@mui/material/Typography'
 import ManoIzquierda from '../../Assents/Mano1.png'
-import ModeEditIcon from '@mui/icons-material/ModeEdit';
+import ModeEditIcon from '@mui/icons-material/ModeEdit'
 import IconButton from '@mui/material/IconButton'
 
 const VerPerfil = () => {
+  const auth = useSelector(state => state.auth)
 
-    const auth = useSelector(state=>state.auth)
+  console.log(auth)
 
-    console.log(auth);
-
-
-  return (  <div>
-    <div><Typography variant='h3' color='primary'>Perfíl</Typography></div>
-    <img style={{width:"80px",margin:"20px"}} src={ManoIzquierda}></img>
-    <div style={{margin:"10px"}}>
-        <Typography variant='h5' color='primary' >Correo</Typography>
-        {auth.email && <Typography variant='h7' >{auth.email}</Typography>}
-    </div>
-    <div style={{margin:"10px"}}>
+  return (
+    <div>
+      <div><Typography variant='h3' color='primary'>Perfíl</Typography></div>
+      <img style={{ width: '80px', margin: '20px' }} src={auth.imagenUrl} className='rounded-full' />
+      <div style={{ margin: '10px' }}>
+        <Typography variant='h5' color='primary'>Correo</Typography>
+        {auth.email && <Typography variant='h7'>{auth.email}</Typography>}
+      </div>
+      <div style={{ margin: '10px' }}>
         <Typography variant='h5' color='primary'>Nombre</Typography>
         {auth.nombres && <Typography variant='h7'>{auth.nombres}</Typography>}
-        <IconButton color='primary'><ModeEditIcon/></IconButton> 
-        </div>
+        <IconButton color='primary'><ModeEditIcon /></IconButton>
+      </div>
     </div>
-    )
-};
+  )
+}
 
-export default VerPerfil;
+export default VerPerfil
